fa7597ebc6b186c035b33e4573dfa5bb43de8a3f
Brittany-Maraneta <Brittany-june@hotmail.com>
2024-02-27 11:12:27 -0700

    main pg

--- client.js
+++ client.js
@@ -41,3 +41,51 @@ window.addEventListener("scroll", function () {
 function scrollWin() {
   window.scrollBy(0, 660);
 }
+
+//TODO create a carousel that loops forever
+document.addEventListener("DOMContentLoaded", function () {
+  const carouselSlide = document.querySelector(".carousel-slide");
+  const images = document.querySelectorAll(".carousel-slide img");
+
+  let counter = 0;
+  const size = images[0].clientWidth;
+
+  function nextSlide() {
+    if (counter >= images.length - 1) {
+      counter = 0;
+    } else {
+      counter++;
+    }
+    carouselSlide.style.transition = "opacity 0.5s ease-in-out";
+    carouselSlide.style.opacity = 0;
+    setTimeout(() => {
+      carouselSlide.style.transition = "none";
+      carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+      carouselSlide.style.opacity = 1;
+    }, 500);
+  }
+
+  carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+
+  setInterval(nextSlide, 3000);
+});
+
+// TODO make a chat popup
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+function myFunction() {
+  let checkBox = document.getElementById("myCheck");
+  let text = document.getElementById("text");
+
+  if (checkBox.checked == true) {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
